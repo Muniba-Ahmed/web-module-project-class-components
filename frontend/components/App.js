c7173a3e3fdcd53c4347bdd980e77037f1a44f1d
Muniba Ahmed <mahmed93@berkeley.edu>
2022-05-09 17:01:18 -0700

    worked on dom ux design

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,8 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Muniba's Todo's</h1>
+        <ul> 
+          <li> walk the dog </li>
+          <li> take out the trash</li>
+          <li> Workout</li>
+        </ul>
+        <form>
+          <input/>
+          <button> Add </button>
+        </form>
+         <button> Clear </button>
       </div>
+    
     )
   }
 }
